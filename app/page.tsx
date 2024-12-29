b234b241c563ef77f100a5abcd524dd2f641e8e0
Izabela Magiera <hello@izabelamagiera.com>
2024-12-29 11:13:04 +0000

    static page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import styles from './page.module.css';
+import Image from 'next/image';
+import s from './page.module.css';
 
 export default function Home() {
-	return <div>home</div>;
+	return (
+		<div className={s.pageWrap}>
+			<div className={s.pageContent}>
+				<Image src="/stampSun.svg" alt="logo" width={80} height={80} />
+				<h1>izabela magiera</h1>
+				<p>this website is work in progress</p>
+			</div>
+		</div>
+	);
 }
