9fd71c1131366f92a5046d9bfa5491d7c42dac50
Izabela Magiera <hello@izabelamagiera.com>
2025-01-24 17:28:26 +0000

    test datasets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import s from './page.module.css';
 import AnimateLogo from './components/AnimateLogo';
+import { getHomepage } from './sanity/pages/homepage-query';
 
-export default function Home() {
+export default async function Home() {
+	const page = await getHomepage();
+	console.log(page, 'check query');
 	return (
 		<div className={s.pageWrap}>
 			<div className={s.pageContent}>
@@ -10,6 +13,7 @@ export default function Home() {
 				<p>
 					<span className={s.bold}>status</span>: work_in_progress
 				</p>
+				<p>{page && page.title}</p>
 			</div>
 		</div>
 	);
---
+++ app/sanity/pages/homepage-query.ts
@@ -0,0 +1,15 @@
+import { cache } from 'react';
+import { clientFetch } from '../client';
+
+export async function getHomepage() {
+	return clientFetch(
+		`
+        *[_type == "homepage"][0] {
+        _id,
+            title,
+    
+        }`,
+		{},
+		['homepage']
+	);
+}
