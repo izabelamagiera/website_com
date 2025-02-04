bba308a8b29092c325e0e6319c100e87b4eb0da8
Izabela Magiera <hello@izabelamagiera.com>
2025-02-04 09:37:11 +0000

    update cv upload button

--- app/components/SectionHeader/SectionHeader.tsx
+++ app/components/SectionHeader/SectionHeader.tsx
@@ -1,26 +1,29 @@
-import { FlexRow } from '@/app/styles';
-
-import { SectionHeaderProps } from '@/app/types/components/SectionHeaderType';
-import React from 'react';
-import { StyledSectionHeader } from './SectionHeader.styled';
-import { HeaderLg, HeaderSm } from '@/app/styles/utils/Typography';
+"use client";
+import { SectionHeaderProps } from "@/app/types/components/SectionHeaderType";
+import React, { useState } from "react";
+import { StyledSectionHeader } from "./SectionHeader.styled";
+import { HeaderLg, HeaderSm } from "@/app/styles/utils/Typography";
 
 export default function SectionHeader({
-	title,
-	label,
-	attachment,
+  title,
+  label,
+  attachment,
 }: SectionHeaderProps) {
-	return (
-		<StyledSectionHeader>
-			<HeaderLg>{title.toUpperCase()}</HeaderLg>
+  const [linkToFile, setLinkToFile] = useState<string>("");
+  const handleDownloadFile = () => {
+    setLinkToFile(attachment ? attachment.url : "");
+  };
+  return (
+    <StyledSectionHeader>
+      <HeaderLg>{title.toUpperCase()}</HeaderLg>
 
-			{attachment && (
-				<HeaderSm>
-					<a href={attachment?.url} target="_blank">
-						{label}
-					</a>
-				</HeaderSm>
-			)}
-		</StyledSectionHeader>
-	);
+      {attachment && (
+        <HeaderSm>
+          <a onClick={handleDownloadFile} href={linkToFile} target="_blank">
+            {label}
+          </a>
+        </HeaderSm>
+      )}
+    </StyledSectionHeader>
+  );
 }
