3000d6dabbe154362918318fa1417d316accefde
Izabela Magiera <hello@izabelamagiera.com>
2025-02-16 13:20:42 +0000

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Loading from './components/utils/Loading/Loading';
 import StyledComponentsRegistry from './registry';
 import { ColorThemeProvider } from './context/ColorContext/ColorThemeContext';
 import GlobalStyle from './styles/GlobalStyle';
+import Script from 'next/script';
 
 export default function RootLayout({
   children
@@ -14,6 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="GTM"
+        src="https://www.googletagmanager.com/gtag/js?id=G-R76G802Q3J"
+        strategy="afterInteractive"
+        data-categoty="analytics"
+        data-service="GTM"
+      />
+      <Script id="google" strategy="afterInteractive" data-categoty="analytics" data-service="GA4">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-R76G802Q3J');`}
+      </Script>
       <StyledComponentsRegistry>
         <ColorThemeProvider>
           <GlobalStyle />
